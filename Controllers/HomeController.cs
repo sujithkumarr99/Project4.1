9339e0189eec4f59683f11f7a8abea5ddaa6af3e
SUJITH KUMAR R <89589374+sujithkumarr99@users.noreply.github.com>
2023-02-23 09:26:53 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Jayvankal.Models;
+
+namespace Jayvankal.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+    public IActionResult Courses()
+    {
+        return View();
+    }
+    public IActionResult Facilities()
+    {
+        return View();
+    }
+    public IActionResult TeachingMethod()
+    {
+        return View();
+    }
+    public IActionResult Testimonials()
+    {
+        return View();
+    }
+    public IActionResult Culture()
+    {
+        return View();
+    }
+    public IActionResult Admission()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
